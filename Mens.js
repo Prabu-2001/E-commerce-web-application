7537d37f9a908c364dda072f793a3c1694d71517
PRABU A S <144416894+Prabu-2001@users.noreply.github.com>
2023-12-14 16:06:58 +0530

    Add files via upload

---
+++ Mens.js
@@ -0,0 +1,60 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function Mens(){
+    return(
+        <>
+            <body className="mens_body">
+                <h1 className="mens_heading" align="center">Mens</h1>
+                <div className="mens_webpage">
+                    <div className="mens_shirts">
+                        <h1>Formal-Shirts:-</h1>
+                        <div className="shirt_img">
+                            <div className="img"><img src="https://th.bing.com/th/id/OIP.QAni0bG5eAbgYm0vLJyGFwHaJo?pid=ImgDet&w=200&h=260&c=7&dpr=1.3" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                            <div className="img"><img src="https://ae01.alicdn.com/kf/HTB157Z1mwoQMeJjy0Fpq6ATxpXaH/Jamickiki-Brand-Mens-Fashion-Dress-Shirts-2017-High-Streetwear-Side-Button-Design-Long-Sleeve-Slim-Fit.jpg" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                            <div className="img"><img src="https://th.bing.com/th/id/OIP.fkC4Z6N_4XSnSf4drvNkGgHaJd?pid=ImgDet&w=200&h=256&c=7&dpr=1.3" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                            <div className="img"><img src="https://th.bing.com/th/id/OIP.mgZZwEJz3P-gcV2Oegp-DAHaHa?w=202&h=202&c=7&r=0&o=5&dpr=1.3&pid=1.7" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                        </div>
+                    </div>
+                    <div className="mens_shirts">
+                        <h1>T-Shirts:-</h1>
+                        <div className="shirt_img">
+                            <div className="img"><img src="https://th.bing.com/th/id/R.81c1b8c9e287e3b040803b75063bcbc2?rik=EWgaXR6PDTShwA&riu=http%3a%2f%2fassets.myntassets.com%2fassets%2fimages%2f1740080%2f2017%2f1%2f27%2f11485500704144-Campus-Sutra-Men-Grey-Melange-Printed-Round-Neck-T-Shirt-6491485500703986-2.jpg&ehk=RhI%2fQRhxMrVLIDVt0TY9G5qX7DUe8XDAgKtYGT5UFrY%3d&risl=&pid=ImgRaw&r=0" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                            <div className="img"><img src="https://ohtopten.com/wp-content/uploads/2015/08/Best-T-shirts-for-men-1.jpg" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                            <div className="img"><img src="https://www.dhresource.com/0x0/f2/albu/g10/M01/07/BB/rBVaVl7_9GOADM0UAACZD0KZqlk918.jpg" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                            <div className="img"><img src="https://images.meesho.com/images/products/95382557/hnuy0_512.jpg" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                        </div>
+                    </div>
+                    <div className="mens_shirts">
+                        <h1>Pants:-</h1>
+                        <div className="shirt_img">
+                            <div className="img"><img src="https://th.bing.com/th/id/OIP.c9CkjqdTCdRE0dWFjl6QEAHaJ4?w=202&h=269&c=7&r=0&o=5&dpr=1.3&pid=1.7" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                            <div className="img"><img src="https://th.bing.com/th/id/OIP.EWnEbnmJYWzOdmcZwObTDAHaJ4?w=202&h=269&c=7&r=0&o=5&dpr=1.3&pid=1.7" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                            <div className="img"><img src="https://th.bing.com/th/id/OIP.ZO2VOZdUwTtwmrrgGtgqVgHaJ4?w=202&h=269&c=7&r=0&o=5&dpr=1.3&pid=1.7" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                            <div className="img"><img src="https://th.bing.com/th/id/OIP.mGc8_bD263GfhNE0cYagegHaJ4?w=202&h=269&c=7&r=0&o=5&dpr=1.3&pid=1.7" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                        </div>
+                    </div>
+                    <div className="mens_shirts">
+                        <h1>Shoes & Sneakers:-</h1>
+                        <div className="shirt_img">
+                            <div className="img"><img src="https://i.pinimg.com/originals/2e/28/ff/2e28ff53eb53e63da954ddb6a3a08a27.jpg" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                            <div className="img"><img src="https://ebestpicks.com/wp-content/uploads/comfortable-mens-dress-shoes-laspen-1024x1024.jpg" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                            <div className="img"><img src="https://images.naptol.com/usr/local/csp/staticContent/product_images/horizontal/750x750/NikeZoom-33-Blue-3.jpg" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                            <div className="img"><img src="https://th.bing.com/th/id/OIP.lk6p0wdggUQItz8QwCHyIQHaHa?pid=ImgDet&rs=1" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                        </div>
+                    </div>
+                    <div className="mens_shirts">
+                        <h1>Sandals & Crocs</h1>
+                        <div className="shirt_img">
+                            <div className="img"><img src="https://i.pinimg.com/originals/3c/fa/5b/3cfa5bc2e921d8af93d6d8b66e515166.jpg" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                            <div className="img"><img src="https://cdna.lystit.com/photos/asos/4fd815f1/asos-Black-Gladiator-Sandals.jpeg" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                            <div className="img"><img src="https://ae01.alicdn.com/kf/HTB17oRdQpYqK1RjSZLeq6zXppXao/Crocse-Outdoor-Mens-Sandals-Summer-Beach-Sandals-Men-Clogs-Croc-Shoes-Sandale-Homme-Sandalias-Homem-Erkek.jpg" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                            <div className="img"><img src="https://th.bing.com/th/id/OIP.wNj6LD3tN8Wdv3WTbMMAEQHaHa?pid=ImgDet&w=200&h=200&c=7&dpr=1.3" height={400} width={300}/><div className="content"><h1>SIZE:</h1><p>L,XL,XXL,XXXL</p></div></div>
+                        </div>
+                    </div>
+                </div>
+            </body>
+        </>
+    )
+}
+export default Mens;
